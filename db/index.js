479d5c64cbc8696cf5369e42d384e08153886cbc
kaseycampbell <77555838+kaseycampbell@users.noreply.github.com>
2021-04-28 20:34:44 -0400

    Update index.js
    fix double-copy

--- db/index.js
+++ db/index.js
@@ -26,43 +26,6 @@ async function getAllUsers() {
   return rows;
 }
 
-module.exports = {
-  client,
-  createUser,
-  getAllUsers,
-  getPostsByID,
-};
-const { Client } = require('pg') // imports the pg module
-
-const client = new Client('postgres://localhost:5432/juicebox-dev');
-
-async function createUser({ username, password }) {
-  try {
-    const { rows } = await client.query(`
-      INSERT INTO users(username, password) 
-      VALUES($1, $2) 
-      ON CONFLICT (username) DO NOTHING 
-      RETURNING *;
-    `, [username, password]);
-
-    return rows;
-  } catch (error) {
-    throw error;
-  }
-}
-
-async function getAllUsers() {
-  const { rows } = await client.query(`SELECT id, username FROM users;`);
-
-  return rows;
-}
-
-module.exports = {  
-  client,
-  createUser,
-  getAllUsers,
-}
-
 async function getUserByUsername(username) {
   try {
     const { rows: [user] } = await client.query(`
@@ -117,3 +80,10 @@ async function getPostById(postId) {
     throw error;
   }
 }
+
+module.exports = {
+  client,
+  createUser,
+  getAllUsers,
+  getPostsByID,
+};
